d67a122e0e9f7c07d3deefa28b641af1a45af066
Nguyen Dinh Thuan <thuan.nguyendinh@hust.edu.vn>
2025-12-16 14:14:11 +0700

    demonstrate observer

--- app/src/main/java/com/example/demolifecycle/MainActivity.kt
+++ app/src/main/java/com/example/demolifecycle/MainActivity.kt
@@ -1,71 +1,59 @@
 package com.example.demolifecycle
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.os.PersistableBundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.ViewModel
+import com.example.demolifecycle.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     var score =0
+    val viewModel: ScoreViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,
+            R.layout.activity_main)
+
+        binding.viewModel = viewModel
+
         var btn = findViewById<Button>(R.id.btnInc)
         var txt = findViewById<TextView>(R.id.txtScore)
-        btn.setOnClickListener {
-            score++
-            txt.text=""+score
+//        btn.setOnClickListener {
+//            viewModel.score++
+//            txt.text=""+viewModel.score
+//        }
+        binding.btnInc.setOnClickListener {
+            viewModel.score.value=viewModel.score.value+1
+            //txt.text=""+viewModel.score
         }
-        if(savedInstanceState!=null)
-        {
-            score = savedInstanceState.getInt("score")
-            txt.text=""+score
+        val score_Observer = Observer<Int> { newValue ->
+            binding.txtScore.text = newValue.toString()
         }
-        Log.d("TESTLC","CREATED")
-        lifecycle.addObserver(MyObserver())
+        viewModel.score.observe(this,score_Observer)
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt("score",score)
-    }
-//    override fun onStart() {
-//        super.onStart()
-//        Log.v("TESTLC","STARTED")
-//        Log.i("TESTLC","STARTED")
-//        Log.d("TESTLC","STARTED")
-//        Log.e("TESTLC","STARTED")
-//    }
-//    override fun onResume(){
-//        super.onResume()
-//        Log.d("TESTLC","RESUMED")
-//    }
-//    override fun onPause(){
-//        super.onPause()
-//        Log.d("TESTLC","PAUSED")
-//    }
-//    override fun onStop(){
-//        super.onStop()
-//        Log.d("TESTLC","STOPPED")
-//    }
-//    override fun onDestroy() {
-//        super.onDestroy()
-//        Log.d("TESTLC","DESTROYED")
-//    }
 }
-class MyObserver : DefaultLifecycleObserver {
-    override fun onStart(owner: LifecycleOwner) {
-        super.onStart(owner)
-        Log.d("TESTLC","STARTED LISTENING IN OBSERVER")
-    }
+class ScoreViewModel : ViewModel() {
+     var score= MutableLiveData<Int>(0)
 }
+
+
